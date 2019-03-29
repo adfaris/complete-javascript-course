48189dba9755ec0c490315c6ad8ad6f4886ca15c
A.D.Faris <adfaris@gmail.com>
2019-03-29 00:26:41 -0400

    variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,12 +1,31 @@
-console.log('I am here')
-let firstName = 'John'
-console.log(firstName)
-let lastName = 'Smith'
-console.log(lastName)
-let age = 28
-let fullAge = true
-console.log(fullAge)
-let job 
-console.log(job)
-let _3name = 3
-// let $name = 'name'
+// console.log('I am here')
+// let firstName = 'John'
+// console.log(firstName)
+// let lastName = 'Smith'
+// console.log(lastName)
+// let age = 28
+// let fullAge = true
+// console.table(fullAge)
+// let job
+// console.log(job)
+// let _3name = 3
+// // let $name = 'name'
+
+// // let wife = prompt('what is my name?')
+// // console.log(wife)
+// let x,y
+// x = y = (6+6) * 3 - 4 // 12*3 = 36-4 = 32
+// console.log(x,y)
+
+
+// BMI = mass / height^2 
+
+let massMark, massJohn, johnHeight, markHeight, markBMI, johnBMI, greaterBMI
+
+markBMI = massMark / (markHeight * markHeight)
+
+johnBMI = massJohn / (johnHeight * johnHeight)
+
+greaterBMI = markBMI > johnBMI
+
+console.log('Is Mark\'s higher than John\'s?' + ' ' + greaterBMI)
