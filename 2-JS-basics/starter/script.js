d3a9a5ee5edb5ae0120fd45510b703a6a3ed52ad
A.D.Faris <adfaris@gmail.com>
2019-03-28 23:22:30 -0400

    variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,12 @@
 console.log('I am here')
+let firstName = 'John'
+console.log(firstName)
+let lastName = 'Smith'
+console.log(lastName)
+let age = 28
+let fullAge = true
+console.log(fullAge)
+let job 
+console.log(job)
+let _3name = 3
+// let $name = 'name'
